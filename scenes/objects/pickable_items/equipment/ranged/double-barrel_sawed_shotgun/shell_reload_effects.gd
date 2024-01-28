35eadfbb88a3470fdd712ecc9617e8a7f9c90568
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2024-01-28 19:30:45 +0100

    Update added for reload ejection shells

--- scenes/objects/pickable_items/equipment/ranged/double-barrel_sawed_shotgun/shell_reload_effects.gd
+++ scenes/objects/pickable_items/equipment/ranged/double-barrel_sawed_shotgun/shell_reload_effects.gd
@@ -1,33 +1,57 @@
 extends Node
 
+export var shells_path : PackedScene
 
-export var impulse_position_1 : Vector3 = Vector3(0, 0.2, 0.1)
-export var impulse_position_2 : Vector3 = Vector3(0, 0.2, 0.1)
+export var ejection_multiplier : float = 1.0
 
-var shotgun_shell = preload("res://scenes/objects/pickable_items/tiny/ammo/shotgun_shells/12-gauge_shotgun_shell.tscn")
+export (NodePath) var shell_position_holder_path
+onready var shell_position_holder : Spatial = get_node(shell_position_holder_path) as Spatial
 
-var all_shell_positions : Array = []
 
-onready var shell_position_1 = $"%ShellPosition1"
-onready var shell_position_2 = $"%ShellPosition2"
+func expell_shells() -> void:
+	print("Expelling shells")
+	set_shell_position()
+
+
+func set_shell_position() -> void:
+	for shell_positions_node in shell_position_holder.get_children():
+		if "Positions" in shell_positions_node.name:
+			var shell_instance : Spatial = shells_path.instance() as Spatial
+			var world_scene
+			if is_instance_valid(GameManager.game):
+				world_scene = GameManager.game
+			else:
+				world_scene = owner.owner_character.owner as Spatial
+			
+			world_scene.add_child(shell_instance)
+			shell_instance.global_translation = shell_positions_node.global_translation
+			shell_instance.global_rotation = shell_positions_node.global_rotation
+			add_impulse_to_shells(shell_instance)
+
+
+func add_impulse_to_shells(shell : Spatial) -> void:
+	shell.apply_impulse(shell.global_translation, Vector3(0.2, 1.0, 0.3) * ejection_multiplier)
+#	shell_2.apply_impulse(shell_2.global_translation, Vector3(-0.2, 1.0, 0.3) * ejection_multiplier)
+
 
 
 func add_shells_to_slot() -> void:
-	var added_new_shells = shotgun_shell.instance()
-	for shells_positions in all_shell_positions:
-		if shells_positions.get_child_count() < 1:
-			shells_positions.add_child(added_new_shells)
+	var added_new_shells = shells_path.instance()
+	for shells_positions_node in shell_position_holder.get_children():
+		if "Positions" in shells_positions_node.name:
+			if shells_positions_node.get_child_count() < 1:
+				shells_positions_node.add_child(added_new_shells)
 
 
 func clear_all_slots() -> void:
-	var added_new_shells = shotgun_shell.instance()
-	for shells_positions in all_shell_positions:
-		for bullet_shells in shells_positions.get_children():
-			bullet_shells.queue_free()
+	for shells_positions_node in shell_position_holder.get_children():
+		if "Positions" in shells_positions_node.name:
+			for bullet_shells in shells_positions_node.get_children():
+				bullet_shells.queue_free()
 
 
 func player_add_shell() -> void:
-	owner.owner_character.player_gun_reload_shells.spawn_bullet_shells(shotgun_shell, Vector3(), Vector3())
+	owner.owner_character.player_gun_reload_shells.spawn_bullet_shells(shells_path, Vector3(), Vector3())
 
 
 func player_clear_shell() -> void:
--- scenes/objects/pickable_items/equipment/ranged/double-barrel_shotgun/reload_shotgun_demo.gd
+++ scenes/objects/pickable_items/equipment/ranged/double-barrel_shotgun/reload_shotgun_demo.gd
@@ -1,33 +1,30 @@
 extends Spatial
 
 export var shells_path : PackedScene
-export var shells_path_2 : PackedScene
 
 export var ejection_multiplier : float = 1.0
 
+export (NodePath) var shell_position_holder_path
+onready var shell_position_holder : Spatial = get_node(shell_position_holder_path) as Spatial
 
 func expell_shells() -> void:
 	print("Expelling shells")
-	var instanced_shell = shells_path.instance()
-	var instanced_shell_2 = shells_path_2.instance()
-	set_shell_position(instanced_shell, instanced_shell_2)
+	set_shell_position()
 
 
-func set_shell_position(instanced_shell_1 : RigidBody , instance_shell_2 : RigidBody) -> void:
-	add_child(instanced_shell_1)
-	add_child(instance_shell_2)
-	
-	instanced_shell_1.global_translation = $"%ShellExpellPositions".global_translation
-	instance_shell_2.global_translation = $"%ShellExpellPositions2".global_translation
-	instanced_shell_1.global_rotation = $"%ShellExpellPositions".global_rotation
-	instance_shell_2.global_rotation = $"%ShellExpellPositions2".global_rotation
-	
-	add_impulse_to_shells(instanced_shell_1, instance_shell_2)
+func set_shell_position() -> void:
+	for shell_positions_node in shell_position_holder.get_children():
+		if "Positions" in shell_positions_node.name:
+			var shell_instance : Spatial = shells_path.instance() as Spatial
+			add_child(shell_instance)
+			shell_instance.global_translation = shell_positions_node.global_translation
+			shell_instance.global_rotation = shell_positions_node.global_rotation
+			add_impulse_to_shells(shell_instance)
 
 
-func add_impulse_to_shells(shell_1 : RigidBody, shell_2 : RigidBody) -> void:
-	shell_1.apply_impulse(shell_1.global_translation, Vector3(0.2, 1.0, 0.3) * ejection_multiplier)
-	shell_2.apply_impulse(shell_2.global_translation, Vector3(-0.2, 1.0, 0.3) * ejection_multiplier)
+func add_impulse_to_shells(shell : Spatial) -> void:
+	shell.apply_impulse(shell.global_translation, Vector3(0.2, 1.0, 0.3) * ejection_multiplier)
+#	shell_2.apply_impulse(shell_2.global_translation, Vector3(-0.2, 1.0, 0.3) * ejection_multiplier)
 
 
 func _on_reload_gun_pressed() -> void:
