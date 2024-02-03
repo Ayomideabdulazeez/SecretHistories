20d7b21fd85fa8a5d7c52b57bf9111d69e5b47a4
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2024-02-03 21:54:25 +0100

    leftover changes

--- scenes/characters/player/player_animations_test.gd
+++ scenes/characters/player/player_animations_test.gd
@@ -6,22 +6,41 @@ extends Node
 "Lee-metford_rifle", "Double-barrel_sawed_shotgun", 
 "Double-barrel_shotgun", "Martini_henry_rifle", "Comet_Shard") var current_weapon = "None": set = change_gun
 
-@export_enum("IDLE", "ADS", "RELOAD") var weapon_state = "IDLE": set = set_weapon_state
+@export_enum("IDLE", "ADS", "RELOAD") var weapon_state = "IDLE":
+	set(value):
+		if not Engine.is_editor_hint():
+			return
+		weapon_state = value
+		if value == "IDLE":
+			do_ads(false)
+			do_idle()
+		
+		elif value == "ADS":
+			do_ads(true)
+		
+		elif value == "RELOAD":
+			get_equipped_weapon().animation_player.play("reload")
+			player_reload()
+			await get_tree().create_timer(get_equipped_weapon().animation_player.get_animation("reload").length - 0.3).timeout
 
 @export var reset_to_idle_pos : bool: set = reset_animation_tree
 @export var ads_weapon_position: Vector3: set = adjust_weapon_position
 @export var ads_weapon_rotation : Vector3: set = adjust_weapon_rotation
 
+
 var spawned_weapon
 
 @onready var inventory = $"../Inventory"
-@onready var main_hand_equipment_root = $"../MainHandEquipmentRoot"
-@onready var animation_tree = %AnimationTree
+
+
+@onready var animation_tree : AnimationTree = %AnimationTree
+
 @onready var arm_position = %MainCharOnlyArmsGameRig.position
 @onready var gun_cam = $"../FPSCamera/SubViewportContainer/SubViewport/GunCam" as Camera3D
+@onready var main_hand_equipment_root = get_node("%MainHandEquipmentRoot")
 
 
-func change_gun(value):
+func change_gun(value : String) -> void:
 	current_weapon = value
 
 	if not Engine.is_editor_hint():
@@ -54,14 +73,14 @@ func change_gun(value):
 		
 	if get_equipped_weapon():
 		get_equipped_weapon().queue_free()
-	%MainHandEquipmentRoot.add_child(spawned_weapon)
-	set_weapon_state("IDLE")
+	main_hand_equipment_root.add_child(spawned_weapon)
+	weapon_state = "IDLE"
 	ads_weapon_position = spawned_weapon.ads_hold_position
 	ads_weapon_rotation = spawned_weapon.ads_hold_rotation
 	spawned_weapon.transform = spawned_weapon.get_hold_transform()
 
 
-func adjust_weapon_position(value):
+func adjust_weapon_position(value) -> void:
 	if not Engine.is_editor_hint():
 		return
 	ads_weapon_position = value
@@ -85,7 +104,7 @@ func adjust_weapon_rotation(value):
 		get_equipped_weapon().transform = get_equipped_weapon().get_hold_transform()
 
 
-func reset_animation_tree(value):
+func reset_animation_tree(value) -> void:
 	if not Engine.is_editor_hint():
 		return
 	reset_to_idle_pos = true
@@ -96,33 +115,13 @@ func reset_animation_tree(value):
 	%AnimationTree.set("parameters/OffHand_MainHand_Blend/blend_amount", 1)
 	%AnimationTree.set("parameters/OffHand_Weapon_States/current", 2)
 
-
-func set_weapon_state(value):
-	notify_property_list_changed()
-	if not Engine.is_editor_hint():
-		return
-	weapon_state = value
-	if value == "IDLE":
-		do_ads(false)
-		do_idle()
-	
-	elif value == "ADS":
-		do_ads(true)
-	
-	elif value == "RELOAD":
-		get_equipped_weapon().animation_player.play("reload")
-		player_reload()
-		await get_tree().create_timer(get_equipped_weapon().animation_player.get_animation("reload").length - 0.3).timeout
-#		do_idle()
-
-
-func player_reload():
+func player_reload() -> void:
 	adjust_arm(Vector3(0.008, -1.364, 0.175))
 	
 	%AnimationTree.set("parameters/" + str(get_equipped_weapon().item_name) + "/active", true)
 
 
-func do_idle():
+func do_idle() -> void:
 	if get_equipped_weapon():
 		if get_equipped_weapon() is GunItem:
 			if get_equipped_weapon().item_size == 0:
@@ -151,17 +150,7 @@ func do_idle():
 				%AnimationTree.set("parameters/OffHand_Weapon_States/current", 0)
 				%AnimationTree.set("parameters/Offhand_Hold_Animation/current", 0)
 
-#		elif get_equipped_weapon().item_size == 3:
-#				%AnimationTree.set("parameters/Hand_Transition/current", 0)
-#				%AnimationTree.set("parameters/OffHand_MainHand_Blend/blend_amount", 1)
-#
-#				%AnimationTree.set("parameters/Weapon_states/current", 0)
-#				%AnimationTree.set("parameters/Hold_Animation/current", 0)
-#
-#				%AnimationTree.set("parameters/OffHand_Weapon_States/current", 0)
-#				%AnimationTree.set("parameters/Offhand_Hold_Animation/current", 0)
-
-func do_ads(value):
+func do_ads(value : bool) -> void:
 	if not Engine.is_editor_hint():
 		return
 	if get_equipped_weapon():
@@ -187,7 +176,6 @@ func do_ads(value):
 						operation_tween(%AnimationTree,
 						"parameters/MediumAds/blend_amount",
 						%AnimationTree.get("parameters/MediumAds/blend_amount"), 1.0, 0.05)
-#						gun_cam.transform = lerp(gun_cam.transform, Vector3(0, 1.538, 0), 0.1)
 						adjust_arm(Vector3(-0.054, -1.571, 0.257))
 					adjust_arm(Vector3(-0.054, -1.571, 0.257))
 			else:
@@ -211,26 +199,28 @@ func do_ads(value):
 						"parameters/MediumAds/blend_amount",
 						%AnimationTree.get("parameters/MediumAds/blend_amount"), 0.0, 0.1)
 						adjust_arm(Vector3(0.008, -1.364, 0.175))
-				$"../FPSCamera".fov = lerp($"../FPSCamera".fov, 70, 0.1)
+				#$"../FPSCamera".fov = lerp($"../FPSCamera".fov, 70, 0.1)
 
 
 func get_equipped_weapon() -> EquipmentItem:
-	var equipped_weapon
-	for available_weapon in %MainHandEquipmentRoot.get_children():
+	var equipped_weapon : Object
+	for available_weapon in main_hand_equipment_root.get_children():
 		if available_weapon is EquipmentItem:
 			equipped_weapon = available_weapon
 	return equipped_weapon
 
+		#animation_tree_tweener("parameters/IdleWalkRun/blend_amount", 0.0, 0.1)
 
-func operation_tween(object : Object, method, tweening_from, tweening_to, duration):
+func operation_tween(object, method, tweening_from, tweening_to, duration) -> void:
 	var tweener = get_tree().create_tween()
-	tweener.tween_property(object, method, tweening_to, duration)\
-	.from(tweening_from)\
-	.set_trans(Tween.TRANS_LINEAR)
+	tweener.tween_property(animation_tree, method, tweening_to, duration)
 
+##Used for interpolation between different animations in the animation tree
+func animation_tree_tweener(animation_property, final_animation_value : float, duration: float) -> void:
+	var tweener = get_tree().create_tween()
+	tweener.tween_property(animation_tree, animation_property, final_animation_value, duration)
 
 # TODO: haven't tested since changing tween
-func adjust_arm(final_position):
+func adjust_arm(final_position : Vector3) -> void:
 	var ads_tween = get_tree().create_tween()
-	ads_tween.tween_property(%MainCharOnlyArmsGameRig, "position", final_position, 0.01) 
-	ads_tween.start()
+	ads_tween.tween_property(%MainCharOnlyArmsGameRig, "position", final_position, 0.01)
